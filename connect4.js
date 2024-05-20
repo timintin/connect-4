eea5454b79499dbf1cc5f5044956fd80115673e9
timintin <153340367+timintin@users.noreply.github.com>
2024-05-19 21:21:56 -0400

    Add files via upload

---
+++ connect4.js
@@ -0,0 +1,146 @@
+class Player {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+}
+
+class Game {
+  constructor(height = 6, width = 7) {
+    this.height = height;
+    this.width = width;
+    this.board = [];
+    this.players = [];
+    this.currPlayer = null; // active player: 1 or 2
+    this.gameOver = false;
+
+    this.makeBoard();
+    this.makeHtmlBoard();
+  }
+
+  makeBoard() {
+    for (let y = 0; y < this.height; y++) {
+      this.board.push(Array.from({ length: this.width }));
+    }
+  }
+
+  makeHtmlBoard() {
+    const board = document.getElementById('board');
+    board.innerHTML = '';
+
+    // make column tops (clickable area for adding a piece to that column)
+    const top = document.createElement('tr');
+    top.setAttribute('id', 'column-top');
+    top.addEventListener('click', this.handleClick.bind(this));
+
+    for (let x = 0; x < this.width; x++) {
+      const headCell = document.createElement('td');
+      headCell.setAttribute('id', x);
+      top.append(headCell);
+    }
+
+    board.append(top);
+
+    // make main part of board
+    for (let y = 0; y < this.height; y++) {
+      const row = document.createElement('tr');
+
+      for (let x = 0; x < this.width; x++) {
+        const cell = document.createElement('td');
+        cell.setAttribute('id', `${y}-${x}`);
+        row.append(cell);
+      }
+
+      board.append(row);
+    }
+  }
+
+  findSpotForCol(x) {
+    for (let y = this.height - 1; y >= 0; y--) {
+      if (!this.board[y][x]) {
+        return y;
+      }
+    }
+    return null;
+  }
+
+  placeInTable(y, x) {
+    const piece = document.createElement('div');
+    piece.classList.add('piece');
+    piece.style.backgroundColor = this.currPlayer.color;
+    piece.style.top = -50 * (y + 2);
+
+    const spot = document.getElementById(`${y}-${x}`);
+    spot.append(piece);
+  }
+
+  endGame(msg) {
+    alert(msg);
+  }
+
+  handleClick(evt) {
+    if (this.gameOver) return;
+
+    // get x from ID of clicked cell
+    const x = +evt.target.id;
+
+    // get next spot in column (if none, ignore click)
+    const y = this.findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
+
+    // place piece in board and add to HTML table
+    this.board[y][x] = this.currPlayer;
+    this.placeInTable(y, x);
+
+    // check for win
+    if (this.checkForWin()) {
+      this.gameOver = true;
+      return this.endGame(`${this.currPlayer.name} wins!`);
+    }
+
+    // check for tie
+    if (this.board.every(row => row.every(cell => cell))) {
+      this.gameOver = true;
+      return this.endGame('Tie!');
+    }
+
+    // switch players
+    this.currPlayer = this.currPlayer === this.players[0] ? this.players[1] : this.players[0];
+  }
+
+  checkForWin() {
+    const _win = cells => cells.every(
+      ([y, x]) => y >= 0 &&
+                  y < this.height &&
+                  x >= 0 &&
+                  x < this.width &&
+                  this.board[y][x] === this.currPlayer
+    );
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+        const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+        const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+        const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+          return true;
+        }
+      }
+    }
+  }
+}
+
+document.getElementById('start-game').addEventListener('click', () => {
+  const p1Color = document.getElementById('p1-color').value || 'red';
+  const p2Color = document.getElementById('p2-color').value || 'blue';
+  const player1 = new Player('Player 1', p1Color);
+  const player2 = new Player('Player 2', p2Color);
+
+  const game = new Game();
+  game.players = [player1, player2];
+  game.currPlayer = player1;
+});
